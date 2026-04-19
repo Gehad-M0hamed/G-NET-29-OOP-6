8b92076fc22301ab42f95aa34dd2175ae7d14d86
Gehad Mohamed <gehadmohamedstu@gmail.com>
2026-04-20 01:03:51 +0200

    Q1

--- G-NET-29-OOP-6/Program.cs
+++ G-NET-29-OOP-6/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        //Part 01 : Theoretical Questions
+
+        #region Q1 :
+        //*is the process of exposing only what the user needs and hiding how it is implemented.
+        //    Abstraction:Remote Control
+        //    What an object does
+
+        //    Encapsulation:TV Casing
+        //    How data is protected
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello");
         }
     }
 }
+
+
