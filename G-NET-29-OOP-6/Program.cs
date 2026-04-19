dbc91ad8a35189d36329bb4dc5199f0e2bf4c4ef
Gehad Mohamed <gehadmohamedstu@gmail.com>
2026-04-20 01:22:19 +0200

    Q2

--- G-NET-29-OOP-6/Program.cs
+++ G-NET-29-OOP-6/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace G_NET_29_OOP_6
+﻿using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace G_NET_29_OOP_6
 {
     internal class Program
     {
@@ -13,7 +16,19 @@
         //    How data is protected
         #endregion
 
-
+        #region Q2:
+        Interface:
+            *What you can do
+            *Defines roles & capabilities
+            * Supports multiple inheritance
+            *It cannot contain a constructor or data fields.
+        Abstract Class:
+            *What you are
+            * Defines identity & shared behavior
+            *It only supports individual inheritance
+            * It can contain a constructor and data fields.
+       Interface (Interface)
+        #endregion
 
 
 
